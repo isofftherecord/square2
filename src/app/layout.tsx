32cb936348b8da3d54d22d832b48bb4601da5f70
katheryn-OTR <katheryn@madeotr.com>
2026-09-08 11:48:12 -0500

    fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ const signifier = localFont({
   src: [
     { path: "../fonts/TestSignifier-Extralight.otf", weight: "200", style: "normal" },
     { path: "../fonts/TestSignifier-Light.otf", weight: "300", style: "normal" },
-    { path: "../fonts/TestSignifier-LightItalic.otf", weight: "300", style: "italic" },
     { path: "../fonts/TestSignifier-Bold.otf", weight: "500", style: "normal" },
     { path: "../fonts/TestSignifier-Regular.otf", weight: "400", style: "normal" },
   ],
